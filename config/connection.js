f92d86271e774aa2f73e5728b87aa6d3668ba85e
Russell H <russellharben@gmail.com>
2019-03-06 14:56:44 -0500

    config

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,14 @@
 var mysql = require('mysql');
-require('custom-env').env('production');
+// require('custom-env').env('production');
 // console.log("Custom ENV", process.env.JAWSDB_URL);
 
 var connection = mysql.createConnection({
-    database: process.env.Database,
-    user: process.env.Username,
-    password: process.env.Password,
-    host: process.env.Host,
-    port: process.env.Port
+    database: "burgers_db",
+    user: "root",
+    password: "root",
+    host: "localhost",
+    port: 3306
 });
-
 
 
 connection.connect(function (err) {
