fb08dbfd53abf77393b10821d8c5b79bec431e30
Russell H <russellharben@gmail.com>
2019-03-06 13:28:24 -0500

    comment

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,14 @@
 var mysql = require('mysql');
-// var conn = require('dotenv').config();
-// let env = process.env.JAWSDB_URL || 'development';
-// console.log("jAWS DB = ", conn.JAWSDB_URL);
 
-// if(process.env.JAWSDB_URL){
-//     connection = mysql.createConnection(process.env.JAWSDB_URL);
-// } else {
-    var connection = mysql.createConnection({
-        database: "burgers_db",
-        user: "root",
-        password: "root",
-        host: "localhost",
-        port: 3306
-    });
-// };
+
+var connection = mysql.createConnection({
+    database: "burgers_db",
+    user: "root",
+    password: "root",
+    host: "localhost",
+    port: 3306
+});
+
 
 connection.connect(function (err) {
     if (err) {
