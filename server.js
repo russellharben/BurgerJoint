ce39cc24e08e7415de8b292e603f0cf93c8b17ab
Russell H <russellharben@gmail.com>
2019-03-06 11:00:34 -0500

    server

--- server.js
+++ server.js
@@ -2,9 +2,7 @@ var path = require('path');
 // bring in and instanciate express
 var express = require('express');
 var app = express();
-var env = require('./.env');
 
-console.log(env);
 
 var PORT = process.env.PORT || 3000;
 
