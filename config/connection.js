ea32e7f6acb70ad723ae4a60d1e994dbbbd99400
Russell H <russellharben@gmail.com>
2019-03-06 18:14:14 -0500

    commit

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,17 @@ var mysql = require('mysql');
 // var connection = require('custom-env').env('production');
 // console.log("Custom ENV", process.env.JAWSDB_URL);
 
-var connection = mysql.createConnection({
-    database: "burgers_db",
-    user: "root",
-    password: "root",
-    host: "localhost",
-    port: 3306
-});
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "root",
+        database: "burgers_db"
+    });
+}
 
 
 connection.connect(function (err) {
